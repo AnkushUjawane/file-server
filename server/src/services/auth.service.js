11a229e229fd316947ffb7f717d6546e553b8183
Ankush Ujawane <multiverse1808@gmail.com>
2026-04-16 00:03:53 +0530

    adding auth service

---
+++ server/src/services/auth.service.js
@@ -0,0 +1,32 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+const {User} = require('../models/user.model');
+
+const signup = async (username, password) => {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+        username,
+        password: hashedPassword
+    });
+
+    return user;
+}
+
+const login = async (username, password) => {
+    const user = await User.findOne({where: {username}});
+    if(!user) throw new Error("User not found");
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch) throw new Error("Invalid credentials");
+
+    const token = jwt.sign(
+        { id: user.id, role: user.role },
+        process.env.JWT_SECRET,
+        { expiresIn: "1d" }
+    );
+
+    return {token, user}
+};
+
+module.exports = {signup, login};
