d257fd42823b568ca0b55e16ec376ccab5c0da75
Ankush Ujawane <multiverse1808@gmail.com>
2026-04-15 23:03:12 +0530

    database initialized in app

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const sequelize = require('./config/db');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+sequelize.sync()
+    .then(() => console.log("Database Connected Successfully"))
+    .catch(err => console.log("DB Error: " + err));
+
+app.get('/', (req, res) => {
+    res.json({
+        success: true,
+        message: "file server is running"
+    });
+});
+
+module.exports = app;
