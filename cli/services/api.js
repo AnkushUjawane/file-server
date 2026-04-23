1a588ac2dbe046c9f6f75106a8bc5db5851675e3
Ankush Ujawane <multiverse1808@gmail.com>
2026-04-24 00:14:36 +0530

    adding new feature:logout from fs

--- cli/services/api.js
+++ cli/services/api.js
@@ -1,13 +1,14 @@
 const axios = require("axios");
-const {getToken} = require("../utils/config");
+const { getToken } = require("../utils/config");
 
 const api = axios.create({
     baseURL: "http://localhost:5000/api"
-})
+});
 
 api.interceptors.request.use((config) => {
     const token = getToken();
-    if(token){
+
+    if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
