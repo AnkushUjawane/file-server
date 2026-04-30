2fdbbcbef8febb19d346ddbbf45f3b78d81e90c8
Ankush Ujawane <multiverse1808@gmail.com>
2026-04-30 15:18:52 +0530

    packaged electron app

--- gui/public/electron.js
+++ gui/public/electron.js
@@ -1,16 +1,23 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 let mainWindow;
+const isDev = process.env.NODE_ENV !== "production";
 
 app.whenReady().then(() => {
-    mainWindow = new BrowserWindow({
-        width: 1200,
-        height: 800,
-        webPreferences:{
-            nodeIntegration: true
-        },
-    });
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true,  
+      nodeIntegration: false,  
+    },
+  });
 
+  if (isDev) {
     mainWindow.loadURL("http://localhost:3000");
-    mainWindow.webContents.openDevTools();
-})
+    mainWindow.webContents.openDevTools(); 
+  } else {
+    mainWindow.loadFile(path.join(__dirname, "../client/build/index.html"));
+  }
+});
