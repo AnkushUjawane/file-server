a425b0797f392021d0ea96a1d4a96636b04426aa
Ankush Ujawane <multiverse1808@gmail.com>
2026-04-30 17:20:53 +0530

    changing base url

--- gui/client/src/services/api.js
+++ gui/client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://fileserver-anfjf6gehwetcrcg.southeastasia-01.azurewebsites.net/api",
 })
 
 api.interceptors.request.use((config) => {
