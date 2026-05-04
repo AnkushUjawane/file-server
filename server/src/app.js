bc44a9525c0285e03480c0a4c72d2dbd541b73e2
Ankush Ujawane <multiverse1808@gmail.com>
2026-05-04 14:34:07 +0530

    minor changes: solving cors error

--- server/src/app.js
+++ server/src/app.js
@@ -1,28 +1,36 @@
 const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/db');
+
 const authRoutes = require('./routes/auth.routes');
+const fileRoutes = require('./routes/file.routes');
+const shareRoutes = require("./routes/share.routes");
+
 const authMiddleware = require('./middleware/auth.middleware');
 const errorMiddleware = require('./middleware/error.middleware');
-const fileRoutes = require('./routes/file.routes');
 const Limiter = require('./middleware/rateLimit.middleware');
-const shareRoutes = require("./routes/share.routes")
 
 require('./models/user.model');
 
 const app = express();
 
 app.use(cors());
+app.options("*", cors()); 
+
 app.use(express.json());
+
+app.use(Limiter);
+
 app.use("/api/auth", authRoutes);
 app.use("/api/files", fileRoutes);
 app.use("/api/share", shareRoutes);
+
 app.use(errorMiddleware);
-app.use(Limiter)
 
 sequelize.sync()
     .then(() => console.log("Database Connected Successfully"))
     .catch(err => console.log("DB Error: " + err));
+
 
 app.get('/', (req, res) => {
     res.json({
--- server/src/middleware/rateLimit.middleware.js
+++ server/src/middleware/rateLimit.middleware.js
@@ -3,6 +3,9 @@ const rateLimit = require('express-rate-limit');
 const Limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 100,
+
+    skip: (req) => req.method === "OPTIONS",
+
     message: {
         success: false,
         message: "Too many requests, try again later",
